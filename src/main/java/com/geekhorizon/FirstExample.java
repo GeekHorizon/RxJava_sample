1b70eca319a3597374e8b93d5e9279ac9e8c3290
iris <iris@172.20.10.2>
2019-03-31 17:41:44 +0900

    - reduce  - filter  - flatMap

--- src/main/java/com/geekhorizon/FirstExample.java
+++ src/main/java/com/geekhorizon/FirstExample.java
@@ -1,5 +1,13 @@
 package com.geekhorizon;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
 
 /**
@@ -15,5 +23,39 @@ public class FirstExample {
 	public static void main(String args[]) {
 		FirstExample demo = new FirstExample();
 		demo.emit();
+		
+		flatMap();
+		
+		reduce();
+		
+		filter();
+	}
+	
+	public static void flatMap() {
+		int dan = 3;
+		Observable<String> source = Observable.just(dan)
+				.flatMap(gugu -> Observable.range(1, 9),
+						(gugu, i) -> gugu + " * " + i + " = " + gugu * i);
+		source.subscribe(System.out::println);
+	}
+	
+	public static void reduce() {
+		String[] balls = {"1", "3", "5"};
+		Maybe<String> source =  Observable.fromArray(balls).reduce((ball1, ball2) -> ball2 + "(" +ball1 + ")");
+		source.subscribe(System.out::println);
+	}
+	
+	public static void filter() {
+		List<Pair<String, Integer>> sales = new ArrayList<>();
+		sales.add(Pair.of("TV", 2000));
+		sales.add(Pair.of("Camera", 1000));
+		sales.add(Pair.of("TV", 3000));
+		sales.add(Pair.of("Phone", 900));
+		sales.add(Pair.of("TV", 2000));
+		
+		Observable<Pair<String, Integer>> source = Observable.fromIterable(sales).filter(sale -> StringUtils.equals("TV", sale.getLeft()));
+		
+		source.subscribe(System.out::println);
+		
 	}
 }
